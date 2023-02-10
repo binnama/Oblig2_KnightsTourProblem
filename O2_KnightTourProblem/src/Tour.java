e437c6f207cad7f1071c9253179867268f7dbb0a
binnama <laasestad@yahoo.no>
2023-02-10 19:05:42 +0100

    Much done, result messed up

---
+++ O2_KnightTourProblem/src/Tour.java
@@ -0,0 +1,101 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Tour {
+
+    int n;
+    int Free = 0;
+    int jumpNumber = 1;
+    int used = 2;
+    int route = 3;
+    int B[][]; // Makes an empty board
+
+    // Makes a chessboard with all free 
+    public Tour(int n) {
+
+        this.n = n;
+        B = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                B[i][j] = Free;
+            }
+        }
+    }
+
+    // Queue to hold the numbers
+    Queue<Integer> jump = new LinkedList();
+
+    boolean findRoute(int i, int j) {
+        // Sett opp et sluttpunkt for brettet
+        // n^2
+        if (jumpNumber == (n*n)) {
+//            B[i][j] = route;
+            B[i][j] = jumpNumber;
+            return true;
+        }
+
+        if (jumpNumber == 1) {
+            B[i][j] = 1;
+        }
+        else
+            B[i][j] = used;
+
+        System.out.println(B[i][j]);
+        /*
+        To opp, en høyre - DONE
+        To høyre, en opp - DONE
+        To høyre, en ned - DONE
+        To ned, en høyre - DONE
+        To ned, en venstre - DONE
+        To venstre, en ned - DONE
+        To venstre, en opp - DONE
+        To opp, en venstre - DONE
+        (-) er opp/bakover
+         */
+
+
+        int dI[] = { -2, -1, 1, 2,  2,  1, -1, -2 }; // Hvillen rad.
+        int dJ[] = {  1,  2, 2, 1, -1, -2, -2, -1 }; // Hvilken kolonne
+
+        for (int k = 0; k < 8; k++) {
+            int newI = i + dI[k];
+            int newJ = j + dJ[k];
+
+            if (newI >= 0 && newI < n && newJ >= 0 && newJ < n && B[newI][newJ] == Free) {
+                jumpNumber++;
+//                System.out.println(B[i][j]);
+                if (findRoute(newI, newJ)) {
+                   //B[i][j] = route;
+                   B[i][j] = jumpNumber;
+                   return true;
+
+                }
+            }
+
+            if (newI >= 0 && newI < n && newJ >= 0 && newJ < n && B[newI][newJ] == used) {
+                B[i][j] = Free;
+            }
+        }
+        return false;
+    }
+
+    // Kan ikke ha jumpNumber her! For den teller self var venstre mot høyre
+    // Må lagre tellingen i den rekursive løkka på en måte
+    public String toString() {
+//        String result = "\n";
+        String result = " ";
+        int res;
+        for (int i = 0; i < n; i++) {
+            result += "\n";
+            for (int j = 0; j < n; j++) {
+                if (B[i][j] == route) {
+                    result += B[i][j] + " ";
+                }
+                else
+                  result += B[i][j] + " ";
+            }
+        }
+        return result;
+    }
+}
